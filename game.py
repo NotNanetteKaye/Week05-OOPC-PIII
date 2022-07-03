beb7fef4bdaea97884ae318f54732d3681a1c85f
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-07-03 00:14:01 -0500

    created game_mode module in game.py

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Game:
         pass
 
     def display_welcome(self):
+        print("")
         print("Welcome to Rock Paper Scissors Lizard Spock!")
         print("Each match will be best of three games.")
         print("Use the number keys to enter your selection.")
@@ -15,34 +16,48 @@ class Game:
         pass
 
     def display_rules(self):
+        time.sleep(6)
         print("Rock crushes Scissors")
+        time.sleep(1)
         print("Scissors cuts Paper")
+        time.sleep(1)
         print("Paper covers Rock")
+        time.sleep(1)
         print("Rock crushes Lizard")
+        time.sleep(1)
         print("Lizard poisons Spock ")
+        time.sleep(1)
         print("Spock smashes Scissors")
+        time.sleep(1)
         print("Scissors decapitates Lizard")
+        time.sleep(1)
         print("Lizard eats Paper")
+        time.sleep(1)
         print("Paper disproves Spock")
+        time.sleep(1)
         print("Spock vaporizes Rock")
         print("")
-        time.sleep(1)
+        time.sleep(2)
         pass
 
-    def user_input(self):
-        user_input= int(input("1: single-player 2: multi-player"))
-        if user_input == 1:
-            self.player1= AI("Player1")
-            self.player2 = AI("Player2")
-        elif user_input == 2:
-            self.player1= AI("Player1")
-            self.player2 = AI("Player2")
+    def game_mode(self):
+        print("Which mode would you like to play?")
+        user_input= int(input("Please enter 1 for single-player or 2 for multi-player: "))
+        if user_input == 1 or user_input == 2:
+            if user_input == 1:
+                self.player1= AI("Player1")
+                self.player2 = AI("Player2")
+            elif user_input == 2:
+                self.player1= AI("Player1")
+                self.player2 = AI("Player2")
         else:
-            print("Please enter 1 or 2.")
+            print("Invalid option.")
+            print("Please try again.")
+            self.game_mode()
         pass
 
     def run_game(self):
         self.display_welcome()
-        self.display_rules()
-        self.user_input()
+        # self.display_rules()
+        self.game_mode()
         pass
