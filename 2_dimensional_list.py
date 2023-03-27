8374c06b4c253b6e2d5dd9a5ec874911b26ca564
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-27 18:26:53 -0500

    Created examples for multi-dimensional lists

---
+++ 2_dimensional_list.py
@@ -0,0 +1,18 @@
+myList = [1, 2, 3, 4, 1] # 1 dimensional list = 1 row
+# elements son '1,  2,  3,  4,  1'
+# pero indexes [0] [1] [2] [3] [4] 
+
+
+# a two-dimensional list in Python is a list of lists:
+two_dimensional_list = [[1, 2, 3], [1, 5, 9]] # 2 dimensional list = 2 row
+# result of two_dimensional list:
+#
+#     [0] [1] [2]  the format is 2 (lists) x 3 (elements)
+# [0]  1   2   3
+# [1]  1   5   9
+
+#for num in two_dimensional_list:
+#    print(num)
+
+# how to get the 2nd element of the 1st list?
+print(two_dimensional_list[0][1]) #[row][column]
